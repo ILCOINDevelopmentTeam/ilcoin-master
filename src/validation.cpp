0b053b242a97db309f64eb35a96144beaa343f8b
ILCOIN Dev Team <48028026+ILCOINDevelopmentTeam@users.noreply.github.com>
2021-08-25 12:58:39 -0500

    Fix ConnectMiniBlock block code

--- src/validation.cpp
+++ src/validation.cpp
@@ -3133,10 +3133,11 @@ bool ConnectMiniBlock(const CBlock3& block, CValidationState& state, CBlockIndex
 
             std::vector<CScriptCheck> vChecks;
             bool fCacheResults = fJustCheck; // Don't cache results if we're actually connecting blocks (still consult the cache, though)
-            if (!CheckInputs(tx, state, view, fScriptChecks, flags, fCacheResults, txdata[i], nScriptCheckThreads ? &vChecks : NULL))
+            if (!CheckInputs(tx, state, view, fScriptChecks, flags, fCacheResults, txdata[i], nScriptCheckThreads ? &vChecks : NULL)){
                 LogPrintf("%s - CheckInputs on %s failed with %s\n", __func__, tx.GetHash().ToString(), FormatStateMessage(state));
                 return error("ConnectBlock(): CheckInputs on %s failed with %s",
                     tx.GetHash().ToString(), FormatStateMessage(state));
+            }
             control.Add(vChecks);
         }
 
